c7bf958d67d8797c4570bb62388e6cc79949c2ca
Donte Ladatto <donte279@gmail.com>
2022-05-30 18:15:19 -0500

    commit tree acb1ff0c6f68642e0b6d531263a76a327aeaf7ca

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.filter{ |w| w.chars.sort == @word.chars.sort }     
+    end
+end
